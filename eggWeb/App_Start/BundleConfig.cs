11541332f5665d2b9689996882580e43632bd854
Watermelon <watermelon8157@gmail.com>
2018-04-20 19:34:26 +0800

    first git

---
+++ eggWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace eggWeb
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/Scripts").Include(
+                        "~/Scripts/jquery-2.1.1.min.js",
+                        "~/Scripts/materialize.min.js",
+                        "~/Scripts/init.js"));
+ 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/materialize.min.css",
+                      "~/Content/style.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+                    "~/Scripts/vue.min.js",
+                    "~/Scripts/vue-resource.min.js"));
+ 
+        }
+    }
+}
---
+++ eggWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace eggWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
